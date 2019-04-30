59236ed423f7edb2d98544aa76c57b8c3997f0b6
kamg300 <kamg1134@gmail.com>
2019-04-30 15:01:19 +0100

    Added bootstrap and basic frequency check

--- bin/CSVProcessor.php
+++ bin/CSVProcessor.php
@@ -17,11 +17,16 @@ function processFile(string $fileName) {
     return $data;
 }
 
-function echoTableHeader(int $count) {
+function echoTableHeader(int $count, int $col, bool $asc) {
     echo '<tr>';
     for ($x = 0; $x < $count; $x++) {
-        echo '<th>';
-        echo '<form action="index.php" method="get"><input type="submit" name="column" value="' . $x .'"></form>';
+        echo '<th style="text-align: center;">';
+        $val = ($col !== -1 && !$asc) ? 'false' : 'true';
+        if ($x === $col) {
+            echo '<form action="index.php" method="get"><input type="hidden" name="asc" value="' . $val . '"><input type="submit" name="column" value="' . $x . '"></form>';
+        } else {
+            echo '<form action="index.php" method="get"><input type="hidden" name="asc" value="true"><input type="submit" name="column" value="' . $x . '"></form>';
+        }
         echo '</th>';
     }
     echo '</tr>';
@@ -29,8 +34,8 @@ function echoTableHeader(int $count) {
 
 function echoRawTable(array $csvData) {
     $tableSize = count($csvData[0]);
-    echo '<table>';
-    echoTableHeader($tableSize);
+    echo '<table class="table table-bordered">';
+    echoTableHeader($tableSize, -1, true);
     foreach ($csvData as $key => $value) {
         echo '<tr>';
         foreach ($value as $item) { echo '<td>' . $item . '</td>'; }
@@ -39,16 +44,67 @@ function echoRawTable(array $csvData) {
     echo '</table>';
 }
 
-function echoSortedTable(array $csvData, int $column) {
+function getFrequency(array $arr) {
+    $freqArray = array();
+    foreach ($arr as $key => $value) {
+        if (isset($freqArray[$value])) {
+            $freqArray[$value]++;
+        } else {
+            $freqArray[$value] = 1;
+        }
+    }
+    return $freqArray;
+}
+
+function getHighestFrequency(array $arr) {
+    $highest = 0;
+    foreach ($arr as $key => $freq) {
+        if ($freq > $highest) {
+            $highest = $freq;
+        }
+    }
+    return $highest;
+
+}
+
+/*
+ * Above: ff9191
+ * Average: 91ffa4
+ * Below: ff9191
+ */
+
+function echoSortedTable(array $csvData, int $column, bool $asc) {
     $tableSize = count($csvData[0]);
-    if ($column > $tableSize - 1) {
-        echoRawTable($csvData);
-        return;
-    }
+    if ($column > $tableSize - 1) { echoRawTable($csvData); return; } // Verify Column is in array
     $tempArray = array();
+    foreach ($csvData as $key => $value) { $tempArray[$key] = $value[$column]; } // Generate Array of column values
+    $sort = $asc ? SORT_ASC : SORT_DESC;
+    array_multisort($tempArray, $sort, $csvData);
+    $freqArray = getFrequency($tempArray);
+    $avg = getHighestFrequency($freqArray);
+    $lowAvg = $avg - ($avg/10);
+    $highAvg = $avg + ($avg/10);
+    echo '<table>';
+    echoTableHeader($tableSize, $column, $asc);
     foreach ($csvData as $key => $value) {
-        $tempArray[$key] = $value[$column];
+        echo '<tr>';
+        $counter = 0;
+        foreach ($value as $item) {
+            if ($counter === $column) {
+                $freq = $freqArray[$item];
+                if ($freq < $lowAvg) {
+                    echo '<td style="background: #ff9191">' . $item . '</td>';
+                } else if ($freq > $lowAvg && $freq < $highAvg) {
+                    echo '<td style="background: #91ffa4">' . $item . '</td>';
+                } else {
+                    echo '<td style="background: #9191ff">' . $item . '</td>';
+                }
+            } else {
+                echo '<td>' . $item . '</td>';
+            }
+            $counter++;
+        }
+        echo '</tr>';
     }
-    array_multisort($tempArray, SORT_ASC, $csvData);
-    echoRawTable($csvData);
+    echo '</table>';
 }
--- index.php
+++ index.php
@@ -1,6 +1,8 @@
+<!DOCTYPE html>
 <html lang="en">
 <header>
     <link rel="stylesheet" type="text/css" href="style/style.css">
+    <link rel="stylesheet" type="text/css" href="style/bootstrap.min.css">
 </header>
 <body>
 <?php include_once 'style/header.php'; ?>
@@ -12,7 +14,11 @@ if (isset($_SESSION['filename'])) {
     echo '<div id="data-table" style="padding-left: 1%; padding-right: 1%;">';
     $csvData = processFile($_SESSION['filename']);
     if (isset($_GET['column'])) {
-        echoSortedTable($csvData, $_GET['column']);
+        if (isset($_GET['asc']) && $_GET['asc'] == 'false') {
+            echoSortedTable($csvData, $_GET['column'], true);
+        } else {
+            echoSortedTable($csvData, $_GET['column'], false);
+        }
     } else {
         echoRawTable($csvData);
     }
