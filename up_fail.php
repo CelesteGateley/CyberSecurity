ab709cccf360d1e3c46e6813cd7cc067c2b847f1
kamg300 <kamg1134@gmail.com>
2019-05-03 11:39:36 +0100

    Made set filename remove any active file if empty

--- up_fail.php
+++ up_fail.php
@@ -3,5 +3,7 @@ include_once 'bin/SessionController.php';
 verifySession();
 if (isset($_GET['filename'])) {
     $_SESSION['filename'] = $_GET['filename'];
+} else if ($_SESSION['filname']) {
+    unset($_SESSION['filename']);
 }
 echo '<script type="text/javascript">location.href = "index.php";</script>';
